9bc62fa433a39c4ebd1cecb2f24a4ba6c8e164b0
Francine T. <ASUS@Dayu>
2023-07-20 12:34:05 +0700

    CSS123L OOP Project

--- src/Libs/Lot.java
+++ src/Libs/Lot.java
@@ -12,28 +12,35 @@ package Libs;
  */
 public class Lot {
     
-    //attributes
-    private String description;
-    private String location;
-    private double price;
-    private String status;
+    // attributes
+    private int size; // we can amend "sq. m" or just have the number
+    private String block; // "Block 1-5" or change to [int] if you just want the number
+    private int price; // amend "$". if interested in decimals, use [double]
+    private String status; // "Available", "Sold", "Reserved"
+   
+    // constructor
+    public Lot(int size, String block, int price, String status) {
+        this.size = size;
+        this.block = block;
+        this.price = price;
+        this.status = status;
+    }
     
     // methods
-    public String getDescription() {
-        return description;
+    public int getSize() {
+        return size;
     }
 
-    public String getLocation() {
-        return location;
+    public String getBlock() {
+        return block;
     }
 
-    public double getPrice() {
+    public int getPrice() {
         return price;
     }
 
     public String getStatus() {
         return status;
     }
-    
     
 }
